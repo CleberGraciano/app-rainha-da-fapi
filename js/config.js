84df2622adebeebf16855ec1538ff1fe628959c4
cleberGraciano <cleber_batista2010@hotmail.com>
2025-05-30 20:21:42 -0300

    Removendo Campo de Imagem da API

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Arquivo de configuração centralizada
-const API_BASE_URL = '/api';
-//const API_BASE_URL = 'http://145.79.7.14:8086/api';
+//const API_BASE_URL = '/api';
+const API_BASE_URL = 'http://145.79.7.14:8086/api';
 
 
 function logout() {
